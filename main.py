ad579a02542df99b69f7855f78cd1c021849c89a
erickuu <erickgraterol377gmail.com>
2022-10-20 15:10:57 -0500

    adding feature

--- main.py
+++ main.py
@@ -4,61 +4,44 @@ import pandas as pd
 import numpy as np
 
 
+def open_file_excel():
+    #Open file excel
+    sheet_add = pd.read_excel("output_export/info.xlsx",sheet_name="Hoja2")
+    sheet_value = sheet_add[["ips"]]
+    sheet_value_to_list = sheet_value.values.tolist()
+    value_to_list = np.array(sheet_value_to_list) 
+    value_to_arr = value_to_list.reshape(-1).tolist()
+    return value_to_arr
 
-
-
-
-class Ssh_Mikrotik:
-    #contruct 
-    def __init__(self,name_file,command,output_file):
-        self.name_file = name_file
-        self.command = command
-        self.output_file = output_file
-
-    #Open a Excel   
-    def open_file_excel():
-        #Open file excel
-        sheet_add = pd.read_excel("output_export/info.xlsx",sheet_name="Hoja2")
-        sheet_value = sheet_add[["ips"]]
-        sheet_value_to_list = sheet_value.values.tolist()
-        value_to_list = np.array(sheet_value_to_list) 
-        value_to_arr = value_to_list.reshape(-1).tolist()
-        return value_to_arr
-    #Open a Txt
-    def open_file_txt():
+""" def open_file_txt():
         #Open file txt
         file_add = open("ips.txt","r")
         file_value = file_add.read()
         file_value_to_list = file_value.split(",")
         return file_value_to_list
-    #str to array
-    def create_string_to_vector (): 
-        area_name = []
-        area_border_path = []
-        area_cost_path = []
-        area_name_two = []
-        area_border_path_two = []
-        area_cost_path_two = []
-        cadena2 = cadena.split()
-        if len(cadena2) > 7 :
-            indexes = [0,2,2,3,5,5]
-            for index in sorted(indexes, reverse=False):
-                del cadena2[index]
-            area_name.append(cadena2[0])
-            area_border_path.append(cadena2[1])
-            area_cost_path.append(cadena2[2])
-            area_name_two.append(cadena2[3])
-            area_border_path_two.append(cadena2[4])
-            area_cost_path_two.append(cadena2[5])
-        elif len(cadena2) < 7:
-            indexes = [0,2,2]
-            for index in sorted(indexes, reverse=False):
-                del cadena2[index]
-            area_name.append(cadena2[0])
-            area_border_path.append(cadena2[1])
-            area_cost_path.append(cadena2[2])
-        else:
-            print("error")
+"""
+
+def create_string_to_vector (cadena,area_name,area_border,area_cost,area_name2,area_border2,area_cost2): 
+    cadena2 = cadena.split()
+    if len(cadena2) > 7 :
+        indexes = [0,2,2,3,5,5]
+        for index in sorted(indexes, reverse=False):
+           del cadena2[index]
+        area_name.append(cadena2[0])
+        area_border.append(cadena2[1])
+        area_cost.append(cadena2[2])
+        area_name2.append(cadena2[3])
+        area_border2.append(cadena2[4])
+        area_cost2.append(cadena2[5])
+    elif len(cadena2) < 7:
+        indexes = [0,2,2]
+        for index in sorted(indexes, reverse=False):
+           del cadena2[index]
+        area_name.append(cadena2[0])
+        area_border.append(cadena2[1])
+        area_cost.append(cadena2[2])
+    else:
+        print("error")
     df = pd.DataFrame({'Area_Name':area_name, 'Area_Borde_one':area_border, 'Cost':area_cost })
     write_excel('output_export/info.xlsx','Hoja1',df)
 
@@ -91,8 +74,6 @@ def get_Data(value_to_arr,command):
     for i in range(len(value_to_arr)):
         #Connect to SSH 
         try:
-            dir_output = "output_export/{}.txt"
-            create_file = open(dir_output.format(value_to_arr[i]),"w+")
             client = paramiko.SSHClient()
             client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
             client.connect(value_to_arr[i],username='admin',password='S0m0s_2021')
@@ -109,9 +90,6 @@ def get_Data(value_to_arr,command):
             data_buffer = stdout.read().decode("ascii").replace("\n","")
             data_to_str = str(data_buffer)
             create_string_to_vector(data_buffer,area_name,area_border,area_cost,area_name2,area_border2,area_cost2)
-            create_file.write(data_to_str)
-            create_file.write("")
-            create_file.close()
             client.close()
     command_repeat = input("🪶  Again [y/n] : ")
     if command_repeat == "y":
